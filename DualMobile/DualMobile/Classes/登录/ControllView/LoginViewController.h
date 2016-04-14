7370e4038325381f5df0a873db92958f1139dba5
ZZJQ <zhujinquan@aliyun.com>
2016-04-14 18:44:58 +0800

    创建项目
    创建项目

---
+++ DualMobile/DualMobile/Classes/登录/ControllView/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  DualMobile
+//
+//  Created by QUAN on 16/4/14.
+//  Copyright © 2016年 QUAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ DualMobile/DualMobile/Classes/登录/ControllView/LoginViewController.m
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.m
+//  DualMobile
+//
+//  Created by QUAN on 16/4/14.
+//  Copyright © 2016年 QUAN. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
